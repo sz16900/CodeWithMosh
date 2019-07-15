e5c3cd676bcceeb7145c47179385fcd057722035
sz16900 <sz16900@my.bristol.ac.uk>
2019-07-15 17:52:53 -0400

    working with json files

--- Python/6-Python-Standard-Library/json.py
+++ Python/6-Python-Standard-Library/json.py
@@ -1,9 +1,17 @@
 import simplejson as json
+from pathlib import Path
 
-movies = [
-    {"id": 1, "title": "terminator", "year": 1998},
-    {"id": 2, "title": "toystory", "year": 1999}
-]
+# movies = [
+#     {"id": 1, "title": "terminator", "year": 1998},
+#     {"id": 2, "title": "toystory", "year": 1999}
+# ]
 
-data = json.dumps(movies)
-print(data)
+# data = json.dumps(movies)
+# print(data)
+
+
+# then...
+
+data = Path("movies.json",).read_text()
+movies = json.loads(data)
+print(movies)
